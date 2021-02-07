67f797d1cd4b559dc0d332901e63bc59f132cc88
Naveen <thenvnsahu@gmail.com>
2021-02-08 01:06:20 +0530

    refactored to .tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,49 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Link as GatsbyLink, graphql } from "gatsby"
+import {
+  List,
+  ListItem,
+  LinkBox,
+  LinkOverlay,
+  Heading,
+  Text,
+  Center,
+  Box,
+} from "@chakra-ui/react"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
-
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
+type BlogIndexProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+      }
+    }
+    allMarkdownRemark: {
+      nodes: Array<{
+        excerpt: string
+        fields: {
+          slug: string
+        }
+        frontmatter: {
+          date: string
+          title: string
+          description: string
+        }
+      }>
+    }
   }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      {/* <Bio /> */}
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
 }
-
-export default BlogIndex
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
+        description
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
@@ -87,3 +61,72 @@ export const pageQuery = graphql`
     }
   }
 `
+
+const BlogIndex = ({ data }: BlogIndexProps) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout title={siteTitle}>
+        <SEO title="All posts" />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title={data.site.siteMetadata?.description}>
+      <SEO title="Home" />
+      <Center>
+        <List py={8} px={4} maxW="3xl">
+          {posts.map(post => {
+            const title = post.frontmatter.title || post.fields.slug
+
+            return (
+              <ListItem key={post.fields.slug} my={8}>
+                <Box
+                  className="post-item"
+                  boxShadow="2xl"
+                  p="6"
+                  rounded="md"
+                  bg="white"
+                >
+                  <LinkBox as="article">
+                    <Box as="header" mb={2}>
+                      <Heading
+                        size="md"
+                        sx={{
+                          ".post-item:hover &": {
+                            color: "brand.500",
+                          },
+                        }}
+                      >
+                        <LinkOverlay as={GatsbyLink} to={post.fields.slug}>
+                          {title}
+                        </LinkOverlay>
+                      </Heading>
+                      <Text fontSize="xs" color="gray">
+                        {post.frontmatter.date}
+                      </Text>
+                    </Box>
+
+                    <Text as="section">
+                      {post.frontmatter.description || post.excerpt}
+                    </Text>
+                  </LinkBox>
+                </Box>
+              </ListItem>
+            )
+          })}
+        </List>
+      </Center>
+    </Layout>
+  )
+}
+
+export default BlogIndex
