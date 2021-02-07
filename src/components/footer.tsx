8edd7dccf6c50495d9beee5b7c278a1db77d7ff1
Naveen <thenvnsahu@gmail.com>
2021-02-08 00:24:24 +0530

    refactor

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,52 +1,80 @@
-import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import { Box, Flex, HStack, Text, Link, Icon, Center } from '@chakra-ui/react'
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { Box, Flex, HStack, Text, Link, Icon, Center } from "@chakra-ui/react"
 
-import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
-import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram'
-import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter"
+import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram"
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin"
 
 const query = graphql`
   query FooterQuery {
-      site {
-          siteMetadata {
-              social {
-                  twitter
-                  instagram
-                  linkedin
-              }
-              privacyPolicy
-          }
+    site {
+      siteMetadata {
+        social {
+          twitter
+          instagram
+          linkedin
+        }
+        privacyPolicy
       }
+    }
   }
 `
 
 const Footer = () => {
-    const data = useStaticQuery(query)
-    return (
-        <Box bg="brand.900" w="100%" color="white" p={2}>
-            <Flex justify="space-between" wrap="wrap">
-                <HStack spacing={8} p={4}>
-                    <Link href={data.site.siteMetadata?.social?.twitter} isExternal>
-                        <Icon as={FaTwitter} w={6} h={6}/>
-                    </Link>
-                    <Link href={data.site.siteMetadata?.social?.instagram} isExternal>
-                        <Icon as={FaInstagram} w={6} h={6}/>
-                    </Link>
-                    <Link href={data.site.siteMetadata?.social?.linkedin} isExternal>
-                        <Icon as={FaLinkedin} w={6} h={6}/>
-                    </Link>
-                </HStack>
+  const data = useStaticQuery(query)
+  return (
+    <Box as="footer" bg="brand.900" w="100%" color="white" p={2}>
+      <Flex justify="space-between" wrap="wrap">
+        <HStack spacing={8} p={4}>
+          <Link
+            href={data.site.siteMetadata?.social?.twitter}
+            isExternal
+            sx={{
+              "&:hover": {
+                color: "brand.500",
+              },
+            }}
+          >
+            <Icon as={FaTwitter} w={6} h={6} />
+          </Link>
 
-                <Text p={4}>Written with ❤️️ by back benchers</Text>
+          <Link
+            href={data.site.siteMetadata?.social?.instagram}
+            isExternal
+            sx={{
+              "&:hover": {
+                color: "brand.500",
+              },
+            }}
+          >
+            <Icon as={FaInstagram} w={6} h={6} />
+          </Link>
 
-                <Center p={4}>
-                    <Text mr={2}>© 2020</Text>
-                    <Link href={data.site.siteMetadata?.privacyPolicy} isExternal>Privacy Policy</Link>
-                </Center>
-            </Flex>
-        </Box>
-    )
+          <Link
+            href={data.site.siteMetadata?.social?.linkedin}
+            isExternal
+            sx={{
+              "&:hover": {
+                color: "brand.500",
+              },
+            }}
+          >
+            <Icon as={FaLinkedin} w={6} h={6} />
+          </Link>
+        </HStack>
+
+        <Text p={4}>Written with ❤️️ by back benchers</Text>
+
+        <Center p={4}>
+          <Text mr={2}>© 2020</Text>
+          <Link href={data.site.siteMetadata?.privacyPolicy} isExternal>
+            Privacy Policy
+          </Link>
+        </Center>
+      </Flex>
+    </Box>
+  )
 }
 
 export default Footer
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,75 @@
-import React from 'react'
-import { graphql, useStaticQuery, Link } from 'gatsby'
-import { Box, Flex, HStack, Text, Container, Heading, Divider } from '@chakra-ui/react'
+import React from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import {
+  Box,
+  Flex,
+  HStack,
+  Text,
+  Container,
+  Heading,
+  Divider,
+} from "@chakra-ui/react"
 
 import CatoLogoSvg from "./CatoLogoSvg"
 
+type HeaderProps = {
+  title: string
+  author?: string
+  date?: string
+}
+
 const query = graphql`
   query HeaderQuery {
-      site {
-          siteMetadata {
-              description
-          }
+    site {
+      siteMetadata {
+        description
       }
+    }
   }
 `
 
-const Header = () => {
+const Header = ({ title, author, date }: HeaderProps) => {
   const data = useStaticQuery(query)
   return (
-     <Box bg="brand.500" w="100%" color="white">
-         <Flex direction="row" justify="space-between" p={4}>
-             <Link to="/">
-                 <Text as="strong" fontSize="lg">cato</Text>
-                 <Text as="strong" fontSize="lg" color="brand.900" ml={1}>blog</Text>
-            </Link>
-             <HStack spacing={8}>
-                 <Link to="/"><Text>Home</Text></Link>
-                 <Link to="/authors"><Text>Authors</Text></Link>
-                 <Link to="/archive"><Text>Archive</Text></Link>
-                 <Divider orientation="vertical"></Divider>
-                 <Link to="https://cato.tv"><Text color="brand.900">Cato.tv</Text></Link>
-             </HStack>
-         </Flex>
-         <Container centerContent py={24} px={0}>
-            <CatoLogoSvg />
-            <Heading size="lg" align="center">{data.site.siteMetadata?.description}</Heading>
-         </Container>
-     </Box>
- )
+    <Box bg="brand.500" w="100%" color="white">
+      <Flex direction="row" justify="space-between" p={4}>
+        <Link to="/">
+          <Text as="strong" fontSize="lg">
+            cato
+          </Text>
+          <Text as="strong" fontSize="lg" color="brand.900" ml={1}>
+            blog
+          </Text>
+        </Link>
+        <HStack as="nav" spacing={8}>
+          <Link to="/">
+            <Text>Home</Text>
+          </Link>
+          <Link to="/authors">
+            <Text>Authors</Text>
+          </Link>
+          <Link to="/archive">
+            <Text>Archive</Text>
+          </Link>
+          <Divider orientation="vertical"></Divider>
+          <Link to="https://cato.tv">
+            <Text color="brand.900">Cato.tv</Text>
+          </Link>
+        </HStack>
+      </Flex>
+      <Container centerContent py={24} px={0}>
+        <CatoLogoSvg />
+
+        <Box as="header">
+          <Heading size="lg" align="center">
+            {title}
+          </Heading>
+          {author ? <Text>{author}</Text> : ``}
+          {date ? <Text>{date}</Text> : ``}
+        </Box>
+      </Container>
+    </Box>
+  )
 }
 
 export default Header
